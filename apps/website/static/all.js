b23f9223a4b98a8ed1fa1eaa44ad4de1d22debec
sametmax <lesametlemax@gmail.com>
2016-12-30 12:57:09 +0100

    First draft of money book menu

--- apps/website/static/all.js
+++ apps/website/static/all.js
@@ -2,6 +2,8 @@
 
 require('bootstrap/dist/css/bootstrap.css')
 require("./base/all.js");
+require("./components/money-book-menu/all.js");
 require("./screens/landing_page/all.js");
 require("./screens/login/all.js");
 require("./screens/operations/all.js");
+
---
+++ apps/website/static/components/money-book-menu/all.js
@@ -0,0 +1,6 @@
+
+
+require("./create-1st-book.svg");
+require("./main.sass");
+module.exports = require("./main.js");
+
