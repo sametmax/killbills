b3844fe71c6afd4e5d949ebde841ba1df35940f6
TiTi <as.saal01@gmail.com>
2017-04-17 14:53:50 +0200

    Basic currency data

--- apps/website/apps.py
+++ apps/website/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class WebsiteConfig(AppConfig):
     name = 'website'
+
+    def ready(self):
+        Currency = self.get_model("Currency")
+        Currency.objects.get_or_create(code="EUR")
+        Currency.objects.get_or_create(code="USD")
--- apps/website/models.py
+++ apps/website/models.py
@@ -1,3 +1,34 @@
+import uuid
+
+from djmoney.models.fields import MoneyField
+
 from django.db import models
 
+
 # Create your models here.
+class Currency(models.Model):
+    CODE_CHOICES = (
+        ("EUR", "€"),
+        ("USD", "$"),
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    code = models.CharField(max_length=3, choices=CODE_CHOICES)
+    # TODO: currency full name
+
+
+class MoneyBook(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=32)
+    currency = models.ForeignKey(Currency)
+    initial_balance = MoneyField(
+        max_digits=12,
+        decimal_places=2,
+        default_currency='USD',
+        default=0
+    )
+    current_balance = MoneyField(
+        max_digits=12,
+        decimal_places=2,
+        default_currency='USD',
+        default=0
+    )
