3fc0ecd1b7fb7d27d060864bd21302f562c4c093
sametmax <lesametlemax@gmail.com>
2017-07-24 16:24:15 +0200

    Add button to create MB if no MB on /operations

--- apps/website/static/screens/operations/operations.jsx
+++ apps/website/static/screens/operations/operations.jsx
@@ -6,6 +6,8 @@ import "./operations.sass";
 import MoneyBookMenu from "../../components/money-book-menu/money_book_menu.jsx";
 import AppHeader from "../../components/header/header.jsx";
 
+import { Link } from 'react-router'
+
 var menu = document.getElementById('money-books-menu');
 
 const Viewport = () => {
@@ -16,9 +18,20 @@ const Viewport = () => {
       <AppHeader title="No money book" />
       <div className="container" id="app-content">
 
-          <p className="empty">
-            Create a money book in order to test Kill Bills
-          </p>
+          <div className="empty">
+              <p>Create a money book in order to test Kill Bills:</p>
+
+              <p>
+                <Link className="btn btn-primary new-money-book"
+                      to="/moneybooks/new">
+                    <span className="pull-right">
+                      <span className="glyphicon glyphicon-plus-sign">
+                      </span>
+                    </span>
+                    Create a money book
+                </Link>
+              </p>
+          </div>
 
       </div>
 
